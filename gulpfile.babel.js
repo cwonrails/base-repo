134d21a1c91f61f2c2ea7f2305f60025084c36da
Chris Watson <cwonrails@gmail.com>
2016-04-07 09:20:45 -0500

    Edited gulpfile (intentionally incomplete)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,8 +3,6 @@
 import gulp from 'gulp';
 
 // CSS
-import autoprefixer from 'autoprefixer';
-import csso from 'postcss-csso';
 import flexibility from 'postcss-flexibility';
 import postcss from 'gulp-postcss';
 import stylelint from 'stylelint';
@@ -12,17 +10,12 @@ import stylelint from 'stylelint';
 // HTML
 import htmlmin from 'gulp-htmlmin';
 
-// Images
-import imagemin from 'gulp-imagemin';
-
 // Javascript
 import babel from 'gulp-babel';
 import eslint from 'gulp-eslint';
 
 // Utilities
 import browserSync from 'browser-sync';
-import del from 'del';
-import rename from 'gulp-rename';
 
 // Set up paths
 const paths = {
@@ -44,28 +37,16 @@ const paths = {
   }
 };
 
-// Utility tasks
-const clean = () => del([ 'dist/*' ]);
-export { clean };
-
-const copy = () =>
-  gulp.src('src/*')
-  .pipe(gulp.dest('dist'));
-export { copy };
-
-const reload = browserSync.reload;
-export { reload };
-
-// HTML tasks
+// Minify HTML
 export function html() {
-  return gulp.src(paths.html.src, {since: gulp.lastRun('html')})
+  return gulp.src(paths.html.src)
     .pipe(htmlmin({
-      caseSensitive: true,
+      // caseSensitive: true,
       // collapseBooleanAttributes: true,
       collapseWhitespace: true,
       // conservativeCollapse: true,
       // removeAttributeQuotes: true,
-      keepClosingSlash: true,
+      // keepClosingSlash: true,
       removeComments: true,
       // removeEmptyAttributes: true,
       // removeOptionalTags: true,
@@ -77,56 +58,14 @@ export function html() {
     .pipe(gulp.dest(paths.html.dest));
 }
 
-export function images() {
-  return gulp.src(paths.images.src, {since: gulp.lastRun('images')})
-    .pipe(imagemin({
-    optimizationLevel: 7,
-    progressive: true,
-    interlaced: true,
-    multipass: true
-    }))
-    .pipe(gulp.dest(paths.images.dest));
-}
-
 export function scripts() {
-  return gulp.src(paths.scripts.src, {since: gulp.lastRun('scripts')})
-    .pipe(eslint())
+  return gulp.src(paths.scripts.src)
     .pipe(babel())
-    .pipe(rename({
-      basename: 'main',
-      suffix: '.min'
-    }))
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
-const processors = [
-  stylelint,
-  autoprefixer({browsers: ['last 2 versions']}),
-  flexibility,
-  csso({restructure: false})
-];
-
 export function styles() {
-  return gulp.src(paths.styles.src, {since: gulp.lastRun('styles')})
-    .pipe(postcss(processors))
-    .pipe(rename({
-      basename: 'main',
-      suffix: '.min'
-    }))
+  return gulp.src(paths.styles.src)
+    .pipe(postcss)()
     .pipe(gulp.dest(paths.styles.dest));
 }
-
-export function watch() {
-  browserSync.init({
-    server: 'src'
-    });
-  gulp.watch(paths.html.src).on('change', reload);
-  gulp.watch(paths.images.src).on('change', reload);
-  gulp.watch(paths.scripts.src).on('change', reload);
-  gulp.watch(paths.styles.src).on('change', reload);
-}
-
-const build = gulp.series (html, images, scripts, styles);
-export { build };
-
-export default gulp.series (build, watch);
